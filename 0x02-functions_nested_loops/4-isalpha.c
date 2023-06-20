b99163fdaaeec07540c890f53f73a19bf581cc80
Joseph Ofori <josephofori75@gmail.com>
2023-06-20 11:54:27 -0700

    task 3

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ *_isalpha - checks to see if a variable is an alphabet
+ *Return: 1 if an alphabet or 0 otherwise
+ */
+
+int _isalpha(int c)
+{
+return ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'));
+}
