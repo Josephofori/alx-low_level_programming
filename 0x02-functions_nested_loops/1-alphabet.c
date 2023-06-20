e537b70f4a39a0e12ea245b870826508982d9967
Joseph Ofori <josephofori75@gmail.com>
2023-06-20 07:08:48 -0700

    task 1.1

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- *main -prints lower alphabets
+ *print_alphabet-prints lower alphabets
  *Return: 0
  *
  */
@@ -8,7 +8,7 @@
 void print_alphabet(void)
 {
 char letter;
-for (letter ='a'; letter <= 'z'; letter++)
+for (letter = 'a'; letter <= 'z'; letter++)
 _putchar(letter);
 
 _putchar('\n');
