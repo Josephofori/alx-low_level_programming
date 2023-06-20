a7dce43e39fc4db8b6e1d1ea73ac328899ac2778
Joseph Ofori <josephofori75@gmail.com>
2023-06-20 07:34:40 -0700

    task 2.2

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- *
+ *print_alphabet_x10-prints alpabets 10 times
  *Return:0
  */
 
@@ -9,12 +9,12 @@ void print_alphabet_x10(void)
 int i;
 char letter;
 
-while(i < 10)
+for (i = 0; i < 10; i++)
 {
 for (letter = 'a'; letter <= 'z'; letter++)
+{
 _putchar(letter);
 
 _putchar('\n');
-i++;
 }
 }
