f8637687386132781f8dcb331cc5be86b358ccb7
Joseph Ofori <josephofori75@gmail.com>
2023-06-20 12:33:47 -0700

    task 7

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_last_digit - prints the last digit of a number
+ * @d: the int to extract the last digit from
+ * Return: value of the last digit
+ */
+int print_last_digit(int d)
+{
+int a;
+
+a = d % 10; 
+if (d < 0)
+{
+d = -d;
+}
+else
+{
+_putchar(a + '0');
+}
+return (a);
+}
