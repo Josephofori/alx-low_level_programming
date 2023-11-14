048090916a9be85c53b68f6ef1cbc120dfe1607a
Joseph Ofori <josephofori75@gmail.com>
2023-11-13 17:16:56 -0800

    adv task 2

---
+++ 0x18-dynamic_libraries/cheat.c
@@ -0,0 +1,15 @@
+#include <unistd.h>
+#include <stdlib.h>
+
+/**
+ *printf - entry point
+ *
+ *return - always return 0
+ */
+
+int printf(const char *format, ...)
+{
+write(1, "9 8 10 24 75 9", 14);
+exit(EXIT_SUCCESS);
+return 0;
+}
